e0c930a2d91dd87504909d4c917ddbba04aa577d
Your Name <your-email@example.com>
2026-01-12 21:30:53 +0530

    Allow DummyJSON image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.dummyjson.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
