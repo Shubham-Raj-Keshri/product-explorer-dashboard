3db5193e0c0c50c453170f25e2e5d3dfb27e62bb
Your Name <your-email@example.com>
2026-01-12 21:33:10 +0530

    Disable Next image optimization for Netlify

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.dummyjson.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.dummyjson.com",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
